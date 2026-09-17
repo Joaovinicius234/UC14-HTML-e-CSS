2f2d2ec994697595a8dc5a9e322ca969f8eaff20
Joaovinicius234 <joaovinicius290ff@gmail.com>
2026-09-17 10:23:11 -0300

    Add files via upload

---
+++ UC-15/scripit.js
@@ -0,0 +1,8 @@
+function mudarTexto() {
+    document.getElementByid
+    ("titulo").textcontent = "você clicou no botão!";
+}
+
+function MudaCor() {
+    document.getElementByid("mensagem").style.color = "blue";
+}
